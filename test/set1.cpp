924c102095b7997c5dcad12a4eebdef609e18924
happymeex <32947316+happymeex@users.noreply.github.com>
2023-03-13 02:52:06 -0400

    passing set1 challenges 1-4

--- test/set1.cpp
+++ test/set1.cpp
@@ -1,41 +1,14 @@
 #include "../src/Hex.h"
 #include <algorithm>
 #include <cctype>
+#include <fstream>
+#include <math.h>
 #include <numbers>
 #include <string>
 #include <unordered_map>
 #include <cmath>
 
-const std::unordered_map<char, double> english_char_freqs = {
-    {' ', 0.183},
-    {'e', 0.103},
-    {'t', 0.075},
-    {'a', 0.065},
-    {'o', 0.062},
-    {'n', 0.057},
-    {'i', 0.057},
-    {'s', 0.053},
-    {'r', 0.050},
-    {'h', 0.050},
-    {'l', 0.033},
-    {'d', 0.033},
-    {'u', 0.028},
-    {'c', 0.023},
-    {'m', 0.020},
-    {'f', 0.020},
-    {'w', 0.017},
-    {'g', 0.016},
-    {'p', 0.015},
-};
-
-double mse(const std::unordered_map<char, double> &m1, const std::unordered_map<char, double> &m2){
-    double ret = 0;
-    for (auto it : m1){
-        double diff = it.second - (m2.contains(it.first) ? m2.at(it.first) : 0);
-        ret += diff * diff;
-    }
-    return ret;
-}
+double CHAR_DISTRIBUTION[128];
 
 std::string hex_to_64(hex hx){
     return hexTo64(hx);
@@ -45,35 +18,82 @@ hex fixed_xor(hex hx1, hex hx2){
     return hexor(hx1, hx2);
 }
 
-hex single_byte_xor_cipher(hex hx){
-    int len = hx.raw.length() / 2;
-    double best_mse = 100;
-    hex best;
+void makeCharFreq(){
+    std::ifstream freq{"test/data/char_freqs.txt"};
     for (int i = 0; i < 128; i++){
-        std::string key = std::string(len, i);
-        hex hx_key = toHex(key);
-        hex decrypted = hexor(hx, hx_key);
-        std::unordered_map<char, double> char_freqs;
-        std::string str = decrypted.toString();
-        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
-        for (auto it: english_char_freqs) char_freqs[it.first] = 0;
-        for (char c: str){
-            if (!char_freqs.contains(c)){
-                char_freqs[c] = 0;
-            }
-            char_freqs[c] += ((double) 1)/len;
+        int c; freq >> c;
+        char colon; freq >> colon;
+        double frequency; freq >> frequency;
+        CHAR_DISTRIBUTION[c] = frequency;
+    }
+}
+
+/**
+ * Given a string of ASCII characters, computes the Bhattacharyya coefficient
+ * between the character distribution of the string and the character distribution known to English.
+ *
+ * @returns the Bhattacharyya coefficient. Larger indicates more likely to be English.
+ *      Always between 0 and 1. 
+ */
+double score(std::string s){
+    int len = s.length();
+    double freqs[128]{0};
+    for (auto c: s){
+        if (c > 127){
+            std::cout<<"weird character: " << c << std::endl;
+            continue;
         }
-        double error  = mse(char_freqs, english_char_freqs);
-        if (error < best_mse){
-            best_mse = error;
+        freqs[c] += ((double) 1)/len;
+    }
+    double bc = 0;
+    for (int i = 0; i < 127; i++){
+        bc += sqrt(CHAR_DISTRIBUTION[i] * freqs[i]);
+    }
+    return bc;
+}
+
+
+std::tuple<hex, double, char> single_byte_xor_cipher(hex hx){
+    if (!hx.isValidString()) throw "this hex value represents no ASCII string";
+    int len = hx.raw.length() / 2;
+    double highestScore = 0;
+    hex best;
+    int bestKey = -1;
+    for (int i = 0; i < 128; i++){
+        hex key = toHex(std::string(len, i));
+        hex decrypted = hexor(hx, key);
+        double stringScore = score(decrypted.toString());
+        if (stringScore > highestScore){
             best = decrypted;
+            highestScore = stringScore;
+            bestKey = i;
         }
     }
+    return std::tuple<hex, double, char>{best, highestScore, bestKey};
+}
 
-    return best;
+std::tuple<hex, double, char> detect_single_byte_xor(const std::vector<hex> &v){
+    double highestScore = 0;
+    hex best;
+    int bestKey = -1;
+    for (hex hx: v){
+        try{
+            auto [decrypted, stringScore, key] = single_byte_xor_cipher(hx);
+            if (stringScore > highestScore){
+                highestScore = stringScore;
+                best = decrypted;
+                bestKey = key;
+            }
+        }
+        catch (const char* msg){
+            continue;
+        }
+    } 
+    return std::tuple<hex, double, char>{best, highestScore, bestKey};
 }
 
 int main(int argc, char** argv){
+    makeCharFreq();
     std::string test(argv[1]);
     if (test == "hex_to_64"){
         if (argc != 3) std::cout << "expected 1 argument" << std::endl;
@@ -82,11 +102,28 @@ int main(int argc, char** argv){
 
     else if (test == "single_byte_xor_cipher"){
         if (argc != 3) std::cout << "expected 1 argument" << std::endl;
-        else std::cout << single_byte_xor_cipher(hex{argv[2]}).toString() << std::endl;
+        auto [decrypted, stringScore, key] = single_byte_xor_cipher(hex{argv[2]});
+        std::cout << "key: " << key << std::endl << decrypted.toString() << std::endl;
     }
 
     else if (test == "fixed_xor"){
         if (argc != 4) std::cout << "expected 2 arguments" << std::endl;
         else std::cout << hexor(hex{argv[2]}, hex{argv[3]}).raw << std::endl;
+    }
+
+    else if (test == "detect_single_byte_xor"){
+        if (argc != 2) std::cout << "expected no arguments" <<std::endl;
+        else{
+            std::ifstream file{"test/data/s1c4.txt"};
+            std::vector<hex> v;
+            for (std::string s; file >> s;){
+                //std::cout << s << std::endl;
+                hex input = hex{s};
+                v.push_back(hex{s});
+            }
+            auto [decrypted, stringScore, key] = detect_single_byte_xor(v);
+            std::cout << "key: " << key << std::endl << decrypted.toString() << std::endl;
+            file.close();
+        }
     }
 }
